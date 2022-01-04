6be008d1ae3b89dec4779ca56fa9c7f8db31d576
samuellaisy <96558637+samuellaisy@users.noreply.github.com>
2022-01-04 11:06:12 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://yellow-flower-5c50.samuellaisy.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
